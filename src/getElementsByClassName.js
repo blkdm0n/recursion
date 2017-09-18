ca89b545e58a68ee33e25474fc73825007cba653
Bernard Kitchens <bernardkitchens@Bernards-MacBook-Pro.local>
2017-09-18 15:39:59 -0700

    completed Recursion getElementsByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,37 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+
+//Input:  className, element
+//Output: all elements with the specified className
+//Constraints:  should use recursion
+//Edge:  None 
+
+
+var getElementsByClassName = function(className, element) {
+  //output the elements with the given className
+  var output = [];
+  
+  //if no element is entered the body is the element to be searched
+  element = element || document.body;
+  
+  //if elements classList exists
+  if (element.classList) {
+    //and if it contains the className
+    if (element.classList.contains(className)) {
+      //push that element to the output array
+      output.push(element);
+    }
+  }
+
+  //if the element has children, iterate through the children
+  
+  for (var i = 0; i < element.children.length; i++) {
+    //recursively call getElementsByClassName on the children[i]
+    var childNode = getElementsByClassName(className, element.children[i]);
+    //concat the output of the recursive call to the existing elements in output
+    output = output.concat(childNode);
+  }
+  //return the final array of elements
+  return output;
+  };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,49 @@
 
 // but you don't so you're going to write it from scratch:
 
+//Inputs:  an object
+//Outputs:  a JSON stringified object
+//Constraints:  must use recursive calls to function we will write
+//Edge Cases:  none
+
+
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //output for our stringified obj;
+  var output = [];
+
+  //if typeof the obj is a function or undefined
+  if (typeof obj === 'function' || typeof obj === 'undefined') {
+    //return null
+    return 'null';
+  }
+  //if obj is a number, boolean, or null change the object to a string
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return String(obj);
+  }
+  //if obj is a string, add double quotes to it
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  //if the obj is an array
+  if (Array.isArray(obj)) {
+    //iterate through the elements an call stringifyJSON on each element
+    for (var i = 0; i < obj.length; i++) {
+      //push the results to our output array
+      output.push(stringifyJSON(obj[i]));
+    }
+  return '[' + output + ']';
+  }
+  
+  //if object is an object and NOT an array...iterate with for in loop
+  for (var key in obj) {
+    //if the object is an object and obj[key] exists and is NOT a function
+      if (typeof obj === 'object' && obj[key] !== undefined && typeof obj[key] !== 'function') {
+      //call stringifyJSON on both the key and value in the object
+      output.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+  //return output as a stringified object
+  return '{' + output + '}';  
 };
